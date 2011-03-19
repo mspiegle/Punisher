9eff3f40168c17333b7431a691108615e91a326f
Michael Spiegle <mike@nauticaltech.com>
2011-03-19 00:14:39 -0700

    Out-of-source build dir

---
+++ externals/Library/M/build/src/CMakeFiles/M.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mspiegle/src/cpp/Punisher/externals/Library/M/src/Md5Hash.cxx" "/Users/mspiegle/src/cpp/Punisher/externals/Library/M/build/src/CMakeFiles/M.dir/Md5Hash.cxx.o"
+  "/Users/mspiegle/src/cpp/Punisher/externals/Library/M/src/String.cxx" "/Users/mspiegle/src/cpp/Punisher/externals/Library/M/build/src/CMakeFiles/M.dir/String.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ externals/Library/M/build/src/CMakeFiles/M.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/M.dir/Md5Hash.cxx.o"
+  "CMakeFiles/M.dir/String.cxx.o"
+  "libM.pdb"
+  "libM.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/M.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
